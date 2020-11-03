33d082864520ac91ee0ba573168d94c9e6f18f95
chriszuniga1 <czuniga3177@gmail.com>
2020-11-03 15:01:27 -0600

    CodingChallenge 2

---
+++ CodingChallenge2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingChallenge2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 1;
+            int c = 0;
+            int sum = 0;
+
+            while (c < 4000000)
+            {
+                c = a + b;
+
+                if (c % 2 == 0)
+                {
+                    sum += c;
+                }
+
+                a = b;
+                b = c;
+            }
+
+            Console.WriteLine("The sum of even numbers in the Fibonacci Sequence not exceeding 4 million is {0}", sum);
+        }
+    }
+}
--- Multiples3And5/Program.cs
+++ Multiples3And5/Program.cs
@@ -19,7 +19,6 @@ namespace Multiples3And5
                 }
             }
             Console.WriteLine("The sum of all the multiples of 3 or 5 below 1000 is {0}", sum);
-        }
-        
-    }
-}
+        }//end main  
+    }//end class
+}//end nameSpace
